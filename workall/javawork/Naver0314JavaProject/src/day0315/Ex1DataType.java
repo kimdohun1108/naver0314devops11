e1426cf9d55b34e903dec1288050e70aff53f4a0
kimdohun <kimdohun1108>
2024-03-15 12:19:30 +0900

    3월15일수업

---
+++ workall/javawork/Naver0314JavaProject/src/day0315/Ex1DataType.java
@@ -0,0 +1,27 @@
+package day0315;
+
+public class Ex1DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 짧은주석
+		/*
+		 긴주석
+		 */
+		
+		// 기본 자료형 공부
+		boolean flag = true;
+		boolean flag2;
+		flag2 = false;
+		
+		System.out.println(flag);
+		System.out.println(flag2);
+		System.out.println(!flag);
+		System.out.println(!flag2);
+		
+		System.out.println(flag && flag2);
+		System.out.println(flag || flag2);
+	}
+
+}
+
